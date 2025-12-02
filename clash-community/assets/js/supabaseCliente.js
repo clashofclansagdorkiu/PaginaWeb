f8ffe2f3f75d7e320581c6b6183f3a456a1102e6
Adrian <adriorri2007@gmail.com>
2025-12-02 11:27:15 +0100

    Version1

--- clash-community/assets/js/supabaseCliente.js
+++
@@ -1,9 +0,0 @@
-// Importamos la librería de Supabase desde el CDN (Internet)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// --- CONFIGURACIÓN ---
-const supabaseUrl = 'https://eldydavwmrgdwkppocym.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsZHlkYXZ3bXJnZHdrcHBvY3ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMDg0ODIsImV4cCI6MjA3OTY4NDQ4Mn0.dGvXYbZdnJnFmF0z9HJvavlZdNLV3PYcOxQQF3msb68'
-
-// Creamos la conexión
-export const supabase = createClient(supabaseUrl, supabaseKey)
